40f92152a8cde1103697fbb44e446e83cf81f414
Arthur Bleij <github@achappey.nl>
2023-05-19 11:30:37 +0200

    Update chat and prediction prompts

--- Prompts.cs
+++ Prompts.cs
@@ -4,9 +4,9 @@ namespace CorporAIte;
 
 public static class Prompts
 {
-    public static string ChatName { get; set; } = "Verzin een naam voor deze chat. Gebruik maximaal 1-4 woorden. Geef in je antwoord alleen de naam en voeg geen andere tekst toe.";
+    public static string ChatName { get; set; } = "Verzin een naam voor deze chat. Gebruik maximaal 1-4 woorden. Gebruik nooit de naam Fakton. Geef in je antwoord alleen de naam en voeg geen andere tekst toe.";
 
-    public static string PredictPrompt { get; set; } = @"Voorspel de volgende chatprompt die de gebruiker wil gaan gebruiken in deze chat. Geef 5 opties. Formateer je antwoord als JSON en voeg geen andere tekst toe: {""prompts"": [{""prompt"": """"}] }";
+    public static string PredictPrompt { get; set; } = @"Voorspel de volgende chatprompt die de gebruiker wil gaan gebruiken in deze chat. Geef altijd 5 opties. Formateer je antwoord als JSON en voeg geen andere tekst toe: {""prompts"": [{""prompt"": """"}] }";
 
 
 }
