8fa75f037642695e50c709613284afa747a6b315
Arthur Bleij <github@achappey.nl>
2023-06-18 12:39:09 +0200

    Refactor ChatProfile CreateMap method

--- Profiles/ChatProfile.cs
+++ Profiles/ChatProfile.cs
@@ -7,22 +7,7 @@ public class ChatProfile : Profile
 {
     public ChatProfile()
     {
-        CreateMap<Message, OpenAI.ObjectModels.RequestModels.ChatMessage>()
-           .AfterMap((src, dest) =>
-        {
-            
-            if (src.Role == "user")
-            {
-                if (!string.IsNullOrEmpty(src.Format))
-                {
-                    dest.Content += $" Formateer als {{src.Format}} content";
-                }
-
-                string attributes = $"{src.Emotional}{src.Authoritarian}{src.Concrete}{src.Convincing}{src.Friendly}";
-                dest.Content += attributes;
-            }
-        });
-		
+        CreateMap<Message, OpenAI.ObjectModels.RequestModels.ChatMessage>();
         CreateMap<OpenAI.ObjectModels.RequestModels.ChatMessage, Message>();
 
         CreateMap<Microsoft.Graph.DriveItem, Folder>();
